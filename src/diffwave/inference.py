17a28f9449c07172d738781c5df4bac704e24fc8
Barannik Volodymyr <v.barannik@coworking.local>
2022-05-21 16:37:55 +0300

    fix typos

--- src/diffwave/inference.py
+++ src/diffwave/inference.py
@@ -143,9 +143,9 @@ def run():
                         help='fast sampling procedure')
     main(parser.parse_args())
 
-def run_from_collab():
+def run_from_colab():
     parameters = Namespace(fast='false', model_dir=runconfig_colab.model_dir, output='output.wav', spectrogram_path=runconfig_colab.default_inference_spectrogram)
     main(parameters)
 
 if __name__ == '__main__':
-    run_from_collab()
+    run_from_colab()
